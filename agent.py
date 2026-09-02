40351db3d4e2b221b8c637f69607e768a21d362d
krishavasoya <krishavasoya406@gmail.com>
2026-09-02 16:57:22 +0530

    Refactor agent.py for improved stock analysis

--- agent.py
+++ agent.py
@@ -1,4 +1,3 @@
-#llm,tools,age
 import os
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
@@ -13,8 +12,9 @@ from tools import(
 )
 load_dotenv()  #load environment variables from .env file
 llm=ChatGroq(
-    model="llama-3.3-70b-versatile",
-    api_key=os.getenv("GROQ_API_KEY")
+    model="openai/gpt-oss-120b",
+    # api_key=os.getenv("GROQ_API_KEY")
+    temperature=0
 )
 agent=create_agent(
     model=llm,
@@ -22,7 +22,7 @@ agent=create_agent(
     system_prompt="""
     you are a proffessional stock research analyst.
 
-    use availab;e tools whenever stock information is needed.
+    use available tools whenever stock information is needed.
 
     provide:
     1.company overview
@@ -34,26 +34,6 @@ agent=create_agent(
     give whole report in easy language and short summary like 5 to 6 bullet points
 """
 )
-# def analyze_stock(symbol):
-#     price=get_stock_price(symbol)
-#     fundamentals=get_fundamentals(symbol)
-#     news=get_news(symbol)
-    
-    # prompt=f""" 
-    # Analyze the stock symbol: {symbol}
-    # Current price: {price}
-    # Fundamentals: {fundamentals}
-    # Recent News: {news}
-
-    # Give:
-    # 1.company overview
-    # 2.Financial health
-    # 3.Strengths and weaknesses
-    # 4.Risks
-    # 5.Overall opinion on the stock
-
-    # Give a short summary in 6 to 7 bullet points with each point having 1-2 sentences and line breaks.
-    # """
 
 def analyze_stock(symbol):
     result=agent.invoke({
@@ -80,10 +60,7 @@ def analyze_stock(symbol):
     })
     return result["messages"][-1].content
     
-
-    
-    # response=llm.invoke(prompt)
-    # return response.content
 if __name__ == "__main__":
     symbol=input("Enter stock symbol: ")
     print(analyze_stock(symbol))
+
